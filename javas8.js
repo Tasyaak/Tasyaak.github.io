fc59c7ea8cb9c715045d53d41769d0f04f5cc1fd
Oleg <61349805+Tasyaak@users.noreply.github.com>
2023-11-16 20:08:41 +0300

    Add files via upload

---
+++ javas8.js
@@ -0,0 +1,194 @@
+//jslint browser: true
+//jslint single: true
+//jslint unordered: true
+//jslint this: true
+//global $, jQuery, history, Slapform
+function homeURL() {
+    history.pushState({modal: "close"}, "ModalOut", "./index8.html");
+}
+
+window.onload = function (event) {
+    let stateStatus = event.currentTarget.history.state;
+    if(stateStatus && stateStatus.modal === "open") {
+        homeURL();
+    }
+
+    document.getElementById("forma").addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        let slapform = new Slapform();
+        let infor = document.getElementById("infor");
+        slapform.submit({
+            form: 'qJp4N7SKz',
+            data: {
+                fio: document.getElementById("fio").value,
+                email: document.getElementById("email").value,
+                tel: document.getElementById("tel").value,
+                organ: document.getElementById("organ").value,
+                soob: document.getElementById("soob").value,
+                polit: document.getElementById("polit").value
+            }
+        }).then(function () {
+            infor.style.color = "green";
+            infor.innerHTML = "Данные отправлены";
+            let s = "input:not([type=\"checkbox\"]), #soob";
+            let inps = document.querySelectorAll(s);
+            for (let i = 0; i < inps.length; ++i) {
+                localStorage.removeItem(inps[i].id);
+                inps[i].value = "";
+            }
+            document.getElementById("polit").checked = false;
+        }).catch(function () {
+            infor.style.color = "red";
+            infor.innerHTML = "Данные не отправлены";
+        });
+    });
+
+    document.querySelector(".clsbt").addEventListener("click", function () {
+        history.pushState({modal: "open"}, "ModalIn", "?form=open");
+    });
+
+    let modalPop = document.getElementById("exform");
+    modalPop.addEventListener("click", function (e) {
+        if (e.target === document.getElementById("exform")) {
+            homeURL();
+        }
+    });
+
+    document.addEventListener("keydown", function (e) {
+        if (e.keyCode === 27) {
+            homeURL();
+        }
+    });
+
+    document.querySelector(".close").addEventListener("click", function () {
+        homeURL();
+    });
+
+    $("input:not([type=\"checkbox\"]), textarea").on("change", function () {
+        localStorage.setItem(this.id, this.value);
+    });
+
+    $("input:not([type=\"checkbox\"]), textarea").on("input", function () {
+        localStorage.setItem(this.id, this.value);
+    });
+
+    $("input[type=\"checkbox\"]").on("click", function () {
+        localStorage.setItem(this.id, this.checked);
+    });
+
+    let s = "input:not([type=\"checkbox\"]), textarea";
+    let inps = document.querySelectorAll(s);
+    for (let i = 0; i < inps.length; ++i) {
+        let val = localStorage.getItem(inps[i].id);
+        if (val != null) {
+            inps[i].value = val;
+        }
+    }
+}
+
+window.onpopstate = function (event) {
+    if (event.state && event.state.modal === "open") {
+        document.querySelector(".main_but").click();
+    } else {
+        document.querySelector(".close").click();
+        homeURL();
+    }
+};//jslint browser: true
+//jslint single: true
+//jslint unordered: true
+//jslint this: true
+//global $, jQuery, history, Slapform
+function homeURL() {
+    history.pushState({modal: "close"}, "ModalOut", "./index8.html");
+}
+
+window.onload = function (event) {
+    let stateStatus = event.currentTarget.history.state;
+    if(stateStatus && stateStatus.modal === "open") {
+        homeURL();
+    }
+
+    document.getElementById("forma").addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        let slapform = new Slapform();
+        let infor = document.getElementById("infor");
+        slapform.submit({
+            form: 'qJp4N7SKz',
+            data: {
+                fio: document.getElementById("fio").value,
+                email: document.getElementById("email").value,
+                tel: document.getElementById("tel").value,
+                organ: document.getElementById("organ").value,
+                soob: document.getElementById("soob").value,
+                polit: document.getElementById("polit").value
+            }
+        }).then(function () {
+            infor.style.color = "green";
+            infor.innerHTML = "Данные отправлены";
+
+            let inps = document.querySelectorAll("input:not([type=\"checkbox\"]), #soob");
+            for (let i = 0; i < inps.length; ++i) {
+                localStorage.removeItem(inps[i].id);
+                inps[i].value = "";
+            }
+            document.getElementById("polit").checked = false;
+        }).catch(function () {
+            infor.style.color = "red";
+            infor.innerHTML = "Данные не отправлены";
+        });
+    });
+
+    document.querySelector(".clsbt").addEventListener("click", function () {
+        history.pushState({modal: "open"}, "ModalIn", "?form=open");
+    });
+
+    let modalPop = document.getElementById("exform");
+    modalPop.addEventListener("click", function (e) {
+        if (e.target === document.getElementById("exform")) {
+            homeURL();
+        }
+    });
+
+    document.addEventListener("keydown", function (e) {
+        if (e.keyCode === 27) {
+            homeURL();
+        }
+    });
+
+    document.querySelector(".close").addEventListener("click", function () {
+        homeURL();
+    });
+
+    function editLocStor(el, val) {
+        localStorage.setItem(el, val);
+    }
+
+    $("input:not([type=\"checkbox\"])").on("change", function () {
+        localStorage.setItem(this.id, this.value);
+    });
+
+    $("input:not([type=\"checkbox\"])").on("input", function () {
+        localStorage.setItem(this.id, this.value);
+    });
+
+    $("input[type=\"checkbox\"]").on("click", function () {
+        localStorage.setItem(this.id, this.checked);
+    });
+
+    let inps = document.querySelectorAll("input:not([type=\"checkbox\"]), textarea");
+    for (let i = 0; i < inps.length; ++i) {
+        let val = localStorage.getItem(inps[i].id);
+        if (val != null) {
+            inps[i].value = val;
+        }
+    }
+}
+
+window.onpopstate = function (event) {
+    if (event.state && event.state.modal === "open") {
+        document.querySelector(".main_but").click();
+    } else {
+        document.querySelector(".close").click();
+        homeURL();
+    }
+};
